f8ac816d158279a8c11350cde90df2f59d8bcbaf
feng <1984779164@qq.com>
2024-01-19 20:22:52 +0800

    feat: useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,56 +3,22 @@
  * @Author: 枫
  * @LastEditors: 枫
  * @description: description
- * @LastEditTime: 2024-01-18 22:36:36
+ * @LastEditTime: 2024-01-19 20:22:28
  */
-import React from '/core/React.js'
+import React, { useState } from '/core/React.js'
 
-function Container() {
-  return <div>
-    <Count num={count}></Count>
-    {/* <Count num={20}></Count> */}
-  </div>
-}
-
-let count = 10
-function Count({ num }) {
-  const update = React.update()
+function Count() {
+  const [num, setNum] = useState(1)
+  const [bar, setBar] = useState('')
   function handle() {
-    console.log('click')
-    count++
-    update()
+    setNum(num + 1);
+    setBar(bar + '1')
   }
-  return <div>count: {count} <button onClick={handle}>+</button></div>
-}
-let hidden = true
-function Hide() {
-  const foo = <div>
-    foo
-    <div>child</div>
-    <div>child2</div>
-  </div>
-  const bar = <div>bar</div>
-
-  const update = React.update()
-  function handle() {
-    hidden = !hidden
-    update()
-  }
-
-  return <div>
-    {hidden && bar}
-    <button onClick={handle}>hide</button>
-    {/* <div>{hidden ? foo : bar}</div> */}
-  </div>
+  return <div>count: {num}{bar} <button onClick={handle}>+</button></div>
 }
 
 const App = () => <div id="app">
-  {/* mini-react
-  <br />
-  <Container></Container> */}
-  <Hide></Hide>
   <Count></Count>
-
 </div>;
 // const App = React.createElement('div', { id: 'app' }, 'mini')
 
