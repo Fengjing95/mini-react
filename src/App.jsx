b84523f4659a73435bcf81072dcd227a21161193
feng <1984779164@qq.com>
2024-01-20 16:23:05 +0800

    feat: useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@
  * @Author: 枫
  * @LastEditors: 枫
  * @description: description
- * @LastEditTime: 2024-01-19 20:22:28
+ * @LastEditTime: 2024-01-20 16:16:28
  */
-import React, { useState } from '/core/React.js'
+import React, { useState, useEffect } from '/core/React.js'
 
 function Count() {
   const [num, setNum] = useState(1)
   const [bar, setBar] = useState('')
   function handle() {
     setNum(num + 1);
-    setBar(bar + '1')
+    // setBar(bar + '1')
   }
+
+  useEffect(() => {
+    console.log('change: ', num)
+    return () => {
+      console.log('clear')
+    }
+  }, [num])
+
+  useEffect(() => {
+    console.log('useEffect');
+    return () => {
+      console.log('clear once')
+    }
+  }, [])
+
   return <div>count: {num}{bar} <button onClick={handle}>+</button></div>
 }
 
